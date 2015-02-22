917328cd9762882f09919dc607ed050ca96f9f1b
thebpfeif <b.r.pfeifer@hotmail.com>
2015-02-22 14:25:05 -0600

    Revert "Revert "Adding Ethernet Module to Repo""
    This reverts commit 35b4ccc16c90203b11819a73ede7d96ed6443db7.

---
+++ Ethernet/EthernetModule/EthernetModule.c
@@ -0,0 +1,18 @@
+/*
+ * EthernetModule.c
+ *
+ * Created: 2/7/2015 5:16:59 PM
+ *  Author: Brandon
+ */ 
+
+#include <avr/io.h>
+#include <stdio.h>
+#include <util/delay.h>
+#include "EthernetLibrary.h"
+
+int main(void){
+	Ethernet_Init();  
+	while(1)
+	{
+	}
+}
